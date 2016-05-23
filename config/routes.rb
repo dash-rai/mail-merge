afce21b6f0673c2c5a21190304acec6f77a3e078
Darshan Rai <darshanjrai@gmail.com>
2016-05-24 01:59:12 +0530

    Add root address
    Root address ('/') points to the login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :templates
   devise_for :users
+  as :user do
+    root 'devise/sessions#new'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
