f9b4b7eff727e9ee3e12bfe5ef8839057a3e8971
Darshan Rai <darshanjrai@gmail.com>
2016-06-11 16:26:44 +0530

    Set up email via SparkPost
    Set the email delivery method to 'sparkpost' and add the
    sparkpost_rails initializer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Send emails via sparkpost
+  config.action_mailer.delivery_method = :sparkpost
 end
